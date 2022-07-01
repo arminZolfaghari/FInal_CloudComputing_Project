f67d29aea5d325d0ee3d204c4e4c2045550add15
arminZolfaghari <arminzd1@gmail.com>
2022-07-01 16:02:41 +0430

    fix bug in pass DB_USER, DB_PASS from config file

--- Private Note/src/config/config.js
+++ Private Note/src/config/config.js
@@ -1,7 +1,7 @@
 export const config = {
     "WEB_PORT": 3000,
-    "MONGO_URL": `mongodb://localhost:27017/private_notes`,
+    "MONGO_URL": `mongodb://localhost:27017/private_notes?authSource=admin&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=false`,
     "EXPIRE_TIME": 18000,    //seconds
-    "DB_USER": "admin",
-    "DB_PWD": "admin"
+    "DB_USER": "admin1",
+    "DB_PWD": "admin1"
 }
--- Private Note/src/database/index.js
+++ Private Note/src/database/index.js
@@ -14,15 +14,10 @@ const connectOption = {
 export default async function connectToDB(uri, callback) {
     try {
         mongoose.Promise = global.Promise
-        // const MONGO_URL = config.MONGO_URL
-        // const MONGO_URL_WITH_USER_PASS = MONGO_URL.substring(0, 10) + config.DB_USER + ":" + config.DB_USER + "@" + MONGO_URL.substring(10, MONGO_URL.length)
-        // console.log(MONGO_URL_WITH_USER_PASS)
-        // console.log(config.MONGO_URL)
-        await mongoose.connect(config.MONGO_URL)
-        // await MongoClient.connect(config.MONGO_URL, (err, db) => {
-        //     if (err)
-        //         console.log(err)
-        // })
+        const MONGO_URL = config.MONGO_URL
+        const MONGO_URL_WITH_USER_PASS = MONGO_URL.substring(0, 10) + config.DB_USER + ":" + config.DB_USER + "@" + MONGO_URL.substring(10, MONGO_URL.length)
+        console.log(`MongoDB URL: ${MONGO_URL_WITH_USER_PASS}`)
+        await mongoose.connect(MONGO_URL_WITH_USER_PASS)
     }
     catch (err){
         console.log(err)
